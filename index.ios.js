a7b811ac526cf0cdc6e734639ef74593af9c2c85
Jessica Barclay <jessicabarclay.net@gmail.com>
2017-06-20 14:46:34 +0100

    Update Title to use constant

--- index.ios.js
+++ index.ios.js
@@ -11,22 +11,11 @@ import {
   Text,
   View
 } from 'react-native';
-
+import Title from './src/Title.js'
 export default class beeTesting extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+      <Title />
     );
   }
 }
--- src/Title.js
+++ src/Title.js
@@ -1,13 +1,13 @@
-import React, {Component} from 'react';
-import {View, Text} from 'react-native';
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
 
 
-export default class Title extends Component {
-  render() {
-    return (
-      <View>
-        <Text>Welcome</Text>
-      </View>
-    );
-  }
+const Title = () => {
+  return (
+    <View>
+      <Text>Welcome</Text>
+    </View>
+  );
 }
+
+export default Title;
